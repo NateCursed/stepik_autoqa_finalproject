64808d4cc4480ea10f1056d0f694af4880240460
Alexander Pitenko <natecursed@gmail.com>
2020-12-07 18:50:21 +0200

    add func for try find login link

--- conftest.py
+++ conftest.py
@@ -2,8 +2,10 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en", help="Choose test language")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
@@ -15,5 +17,3 @@ def browser(request):
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-    
